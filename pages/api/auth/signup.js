74d712f3ae1cf48be463d26e33ffa8c421994225
Mochamad Boval <mochamadboval@gmail.com>
2023-04-17 13:01:52 +0700

    Memecah proses ke dalam fungsi

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -4,6 +4,33 @@ import { equalTo, get, orderByChild, query, ref, set } from "firebase/database";
 
 import { database } from "@/helpers/firebase";
 
+function checkInvalidInput(email, name, password) {
+  if (!name) {
+    return "Nama tidak boleh kosong.";
+  } else if (!email) {
+    return "Email tidak boleh kosong.";
+  } else if (!email.includes("@")) {
+    return "Email harus menggunakan simbol @.";
+  } else if (!password || password.length < 8) {
+    return "Kata sandi harus minimal 8 karakter.";
+  }
+}
+
+async function checkEmailExists(email) {
+  const usersRef = ref(database, "users");
+  const emailQuery = query(usersRef, orderByChild("email"), equalTo(email));
+  return await get(emailQuery);
+}
+
+async function createUser(email, name, password) {
+  const newUserRef = ref(database, `users/${randomUUID()}`);
+  await set(newUserRef, {
+    email,
+    name,
+    password,
+  });
+}
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return;
@@ -13,25 +40,13 @@ export default async function handler(req, res) {
   const name = req.body.name.trim();
   const password = req.body.password;
 
-  let invalid = null;
-  if (!name) {
-    invalid = "Nama tidak boleh kosong.";
-  } else if (!email) {
-    invalid = "Email tidak boleh kosong.";
-  } else if (!email.includes("@")) {
-    invalid = "Email harus menggunakan simbol @.";
-  } else if (!password || password.length < 8) {
-    invalid = "Kata sandi harus minimal 8 karakter.";
-  }
-
+  let invalid = checkInvalidInput(email, name, password);
   if (invalid) {
     res.status(422).json({ message: invalid });
     return;
   }
 
-  const usersRef = ref(database, "users");
-  const emailQuery = query(usersRef, orderByChild("email"), equalTo(email));
-  const user = await get(emailQuery);
+  const user = await checkEmailExists(email);
   if (user.exists()) {
     res.status(422).json({ message: "Email sudah terdaftar." });
     return;
@@ -39,12 +54,7 @@ export default async function handler(req, res) {
 
   const hashedPassword = await hash(password, 10);
 
-  const newUserRef = ref(database, `users/${randomUUID()}`);
-  await set(newUserRef, {
-    name,
-    email,
-    password: hashedPassword,
-  });
+  await createUser(email, name, hashedPassword);
 
   res.status(201).json({ message: "Pendaftaran berhasil." });
 }
